da3f18c9050c9aa2a4f589ef43f4a865c14ea076
danny <da.razo@yahoo.com>
2014-05-12 17:25:10 -0500

    c and cpp matrix user interface (capturing)

---
+++ src/matrix2D.cpp
@@ -0,0 +1,104 @@
+// matrix2D.cpp
+// Permite ingresar una matriz de m x n
+// y luego la escribe en un archivo
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+// estructura de la matriz
+typedef struct {
+   int n_rows;       // numero de renglones
+   int n_cols;       // numero de columnas
+   double **matrix;  // matriz de n_rows x n_cols
+} Matrix2D;
+
+// prototipos
+Matrix2D* inputMatrix2D(void);
+int matrix2DToDisk(Matrix2D*);
+int freeMatrix2D(Matrix2D*);
+
+int main() {
+   Matrix2D *matrix_A_ptr;          // matriz a ingresar por el usuario
+
+   matrix_A_ptr = inputMatrix2D();  // ingresar la matriz
+
+   matrix2DToDisk(matrix_A_ptr);    // escribe la matriz en disco
+
+   freeMatrix2D(matrix_A_ptr);      // libera la memoria
+}  // end main
+
+// captura los elementos de la matriz
+Matrix2D* inputMatrix2D(void) {
+   Matrix2D *matrix_in_ptr;
+
+   // asignacion dinamica
+   matrix_in_ptr = (Matrix2D*)calloc(1, sizeof(Matrix2D));
+
+   // interfaz con el usuario
+   cout << "Numero de renglones de la matriz: ";
+   cin >> matrix_in_ptr->n_rows;
+   cout << "Numero de columnas de la matriz: ";
+   cin >> matrix_in_ptr->n_cols;
+
+   // asignacion dinamica
+   matrix_in_ptr->matrix = (double**)calloc(matrix_in_ptr->n_rows,
+                                            sizeof(double*));
+
+   for (size_t k = 0; k < matrix_in_ptr->n_rows; ++k) {
+      matrix_in_ptr->matrix[k] = (double*)calloc(matrix_in_ptr->n_cols,
+                                                 sizeof(double));
+   }  // end for
+
+   // ingresar la matriz
+   for (size_t m = 0; m < matrix_in_ptr->n_rows; ++m) {
+      for (size_t n = 0; n < matrix_in_ptr->n_cols; ++n) {
+         cout << "elemento [" << m << "][" << n << "]: ";
+         cin >> matrix_in_ptr->matrix[m][n];
+      }  // end for
+   }  // end for
+
+   return matrix_in_ptr;
+}  // end of function inputMatrix2D
+
+// escribe la matriz a un archivo de disco
+int matrix2DToDisk(Matrix2D *matrixPtr) {
+   FILE *file_ptr;   // archivo de disco
+
+   // apertura del archivo
+   file_ptr = (FILE*)fopen("Matrix.txt", "w");
+
+   if (file_ptr == NULL) {
+      cout << "Error en la apertura del archivo!";
+      exit(0);
+   }  // end if
+
+   // escritura del archivo
+   //fprintf(file_ptr, "\n");
+
+   for (size_t rows = 0; rows < matrixPtr->n_rows; ++rows) {
+      for (size_t cols = 0; cols < matrixPtr->n_cols; ++cols) {
+         fprintf(file_ptr, "%7.2f\t", 
+                 matrixPtr->matrix[rows][cols]);
+      }  // end for
+      fprintf(file_ptr, "\n");
+   }  // end for
+
+   // cerrar el archivo
+   fclose(file_ptr);
+
+   return 0;   // exito en la escritura
+}  // end of function matrix2DToDisk
+
+// libera la memoria asignada dinamicamente
+int freeMatrix2D(Matrix2D *matrixPtr) {
+
+   // se libera lo mas anidado primero
+   for (size_t k = 0; k < matrixPtr->n_rows; ++k) {
+      free(matrixPtr->matrix[k]);
+   }  // end for
+   free(matrixPtr->matrix);
+   free(matrixPtr);
+
+   return 0;   // exito en la liberacion de memoria
+}  // end of function freeMatrix2D
